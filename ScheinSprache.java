2e0dba22fae98dbf7f9c0794df55186ed6cb61bd
praKtastic <pascal.hintze@gmx.net>
2014-03-19 10:45:20 +0100

    Java Datei added

---
+++ ScheinSprache.java
@@ -0,0 +1,13 @@
+package de.ostfalia.algodat.ss14.ScheinSprache;
+
+public class ScheinSprache {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
